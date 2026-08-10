4d3ecf0e783648fdf40bbc27398eb10d2444cff5
Prem Verma <premv6264@gmail.com>
2026-08-10 15:51:14 +0530

    fix static asset URLs

--- cicr/cicr/settings.py
+++ cicr/cicr/settings.py
@@ -163,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
